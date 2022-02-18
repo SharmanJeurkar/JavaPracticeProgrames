a6b6ca4d485b2a676a2e247374b5e2401005ee59
“SharmanJeurkar” <kirtisjeurkar@gmail.com>
2022-02-18 15:03:05 +0530

    added new exercise file of Circle.java and Circle_Calc.java

---
+++ src/com/company/practice/programes/circles/Circle.java
@@ -0,0 +1,10 @@
+package com.company.practice.programes.circles;
+
+public class Circle
+{
+    public static void main(String[] args) {
+        Circle_Calc circle = new Circle_Calc();
+        circle.area(7);
+        System.out.println(circle.circumference(7));
+    }
+}
